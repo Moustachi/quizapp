ab62e2d3d29808d9c269bb9ef1559b90dd730752
Moustachi <durablehotpocket@gmail.com>
2016-05-21 20:06:15 -0700

    add questions and multiple choices

---
+++ script.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+    console.log( "ready!" )
+
+    $(".question1").hide();
+    $(".question2").hide();
+    $(".question3").hide();
+    $(".question4").hide();
+    $(".question5").hide();
+    
+    $(".question1c").hide();
+    $(".question2c").hide();
+    $(".question3c").hide();
+    $(".question4c").hide();
+    $(".question5c").hide();
+});
