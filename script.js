a5ffb44e22c83ec7ba061f18487abaa7970fa91f
Moustachi <durablehotpocket@gmail.com>
2016-05-26 21:51:00 -0700

    more debugging II

--- script.js
+++ script.js
@@ -1,15 +1,88 @@
-$( document ).ready(function() {
-    console.log( "ready!" )
+var numberCorrect = 0;
+var currentQuestion = 0;
+var questions = [
+    {
+        question: "Which character created the Matrix?",
+        choices: ["Agent Smith", "The Oracle", "The Architect", "The Keymaker"],
+        questionNum: 1,
+        correct: 2,
+    }, {
+        question: "When the machines run the planet, for what purpose do they use the humans?",
+        choices: ["Manual Labor", "Energy", "Experiments", "Circus acts"],
+        questionNum: 2,
+        correct: 1,
+    }, {
+        question: "What proverb is written in Latin over the Oracle’s kitchen door?",
+        choices: ["Wisdom is Silence", "Know Thyself", "All is Vanity", "Carpe Diem"],
+        questionNum: 3,
+        correct: 1,
+    }, {
+        question: "Who makes a deal with Agent Smith to sell out Morpheus in The Matrix?",
+        choices: ["Agent Smith", "Mouse", "Tank", "Cypher"],
+        questionNum: 4,
+        correct: 3,
+    }, {
+        question: "About what year is it in the ravaged real world?",
+        choices: ["1999", "2199", "2060", "5416"],
+        questionNum: 5,
+        correct: 1,
+    }]
+$(document).ready(function() {
+    $(".submitbuttondiv").hide();
 
-    $(".question1").hide();
-    $(".question2").hide();
-    $(".question3").hide();
-    $(".question4").hide();
-    $(".question5").hide();
-    
-    $(".question1c").hide();
-    $(".question2c").hide();
-    $(".question3c").hide();
-    $(".question4c").hide();
-    $(".question5c").hide();
-});
+    function evaluateChoice() {
+        var answer = $("input[type='radio']:checked").val();
+        if (answer == questions[currentQuestion].correct) {
+            numberCorrect++;
+            $(".scoreCount").text(numberCorrect);
+            alert("Correct");
+        } 
+        else {
+            alert("Incorrect");
+        };
+        currentQuestion++;
+    };
+
+    function nextQuestion() {
+        var newQuestion = '<span class="question">'+questions[currentQuestion].question+'</span>';
+        $(".question").remove();
+        $("li").remove();
+        $(".questionsdiv").append(newQuestion);
+        $(".choices").append(generateChoices());
+        $(".questionCount").text(questions[currentQuestion].questionNum);
+    };
+
+    function generateChoices() {
+        for (var i = 0; i < 4; i++) {
+            $(".choices").append(
+                '<li>'+
+                '<input type = "radio" name="option" class="option" value="'+i+'">'+
+                '<span class= appendedChoices>'+questions[currentQuestion].choices[i]+'</span>'+
+                '</li>');
+        }
+    };
+
+    function refresh() {
+        numberCorrect = 0;
+        currentQuestion = 0;
+        var newQuestion = '<span class="question">'+
+                                questions[currentQuestion].question+
+                          '</span>'
+        $(".scoreCount").text(0);
+        $(".questionCount").text(questions[currentQuestion].questionNum);
+        $(".question").remove();
+        $("li").remove();
+        $(".questionsdiv").append(newQuestion);
+        $(".submitbuttondiv").show();
+    };
+    //----------------------NEW GAME BUTTON-----------------------//
+    $(".newgamediv").on("click", ".newgame", function() {
+        refresh();
+        generateChoices();
+    });
+    //----------------------SUBMIT BUTTON-----------------------//
+    $(".submitButton").on("click", function() {
+        evaluateChoice();
+        nextQuestion();
+    });
+}); //document ready closing tag
